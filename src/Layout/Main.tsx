f80bd9ceffcf52b6905dfc21a38b9915d8d5e51a
Zach Lankton <zachlankton@gmail.com>
2023-09-20 09:29:58 -0400

    Coming Soon!! Classic

--- src/Layout/Main.tsx
+++ src/Layout/Main.tsx
@@ -2,9 +2,9 @@ import "./index.css";
 
 export function MainLayout({ Component, pageProps }: any) {
   return (
-    <>
-      <h1>Rezact</h1>
+    <div class=" container max-w-lg border-2 rounded-3xl mt-16 p-8">
+      <h1 class="text-3xl font-bold">Rezact</h1>
       <Component {...pageProps} />
-    </>
+    </div>
   );
 }
--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -1,7 +1,23 @@
 import { MainLayout } from "../Layout/Main";
 
 export function Page() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl ">Coming Soon!</h1>
+      <p class="mt-5">
+        Embrace a modern UI framework that encourages direct data mutations,
+        offers module-level reactivity, and simplifies component design. With
+        Rezact, you get the power of reactivity without the boilerplate. Dive
+        into a seamless development experience tailored for today's web.
+      </p>
+      <p class="mt-5">
+        Check it out on gitub:{" "}
+        <a href="https://github.com/Rezact/Rezact">
+          https://github.com/Rezact/Rezact
+        </a>
+      </p>
+    </>
+  );
 }
 
 export const Layout = MainLayout;
